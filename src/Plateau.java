2fff5274dd145b9ba0c188b845d908afb365dd21
pro <pro@localhost>
2021-11-14 17:15:36 +0100

    Plateau version 2

--- src/Plateau.java
+++ src/Plateau.java
@@ -1,26 +1,39 @@
-class Methods {
-	public void Matrice (int n, int m) {
-		
-		int[][] Dimmension = new int[n][];
-		for (int i=0 ; i<Dimmension.length; i++)
-		    Dimmension[i]=new int[m];
+
+public class Plateau {
 	
-		for(int array=0; array<Dimmension.length; array++) {
-			for(int item=0; item<Dimmension[array].length; item++) {
-				System.out.print(Dimmension[array][item] + "\t");
+	private int n; //nombres de lignes
+	private int m; //nombres de colonne
+	public Plateau(int n, int m) {
+		this.n = n;
+		this.m = m; 
+		
+	}
+	public int getN() {
+		return n;
+	}
+	public void setN(int n) {
+		this.n = n;
+	}
+	public int getM() {
+		return m;
+	}
+	public void setM(int m) {
+		this.m = m;
+	}
+	public int[][] gettableau(){
+		int[][] tableau = new int[n][];
+		for (int i=0 ; i<tableau.length; i++)
+			tableau[i]=new int[m];
+		return tableau;
+	}
+	public void Affichage(int[][] tableau) {
+		for(int array=0; array<tableau.length; array++) {
+			for(int item=0; item<tableau[array].length; item++) {
+				System.out.print(tableau[array][item] + "\t");
 			}
 			System.out.println();
 	}
+			
 	
 }
-}
-public class Plateau {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Methods run = new Methods();
-		run.Matrice(5,5);
-		
-	}
-
 }
